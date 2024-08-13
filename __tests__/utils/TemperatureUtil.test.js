865307b8bba5813d1796b6f87759056abe0e1c62
Said Omar Rodriguez Peralta <A01562959@tec.mx>
2024-08-12 18:17:03 -0600

    first commit

---
+++ __tests__/utils/TemperatureUtil.test.js
@@ -0,0 +1,43 @@
+import { TemperatureVO } from '../../valueobjects/TemperatureVO.JS';
+import TemperatureUtil from '../../utils/TemperatureUtil';
+
+describe("TemperatureUtil unit tests", () => {
+    const { convert } = TemperatureUtil();
+
+    test('Given Temperature 20C When convert Then 68F', () => {
+        // GIVEN
+        let input = new TemperatureVO(20, "CELSIUS");
+        let expectedOutput = new TemperatureVO(68, "FAHRENHEIT");
+
+        // WHEN
+        let actualOutput = convert(input, "FAHRENHEIT");
+
+        // THEN
+        expect(actualOutput).toStrictEqual(expectedOutput);
+    });
+
+    test('Given Temperature 68F When convert Then 20C', () => {
+        // GIVEN
+        let input = new TemperatureVO(68, "FAHRENHEIT");
+        let expectedOutput = new TemperatureVO(20, "CELSIUS");
+
+        // WHEN
+        let actualOutput = convert(input, "CELSIUS");
+
+        // THEN
+        expect(actualOutput).toStrictEqual(expectedOutput);
+    });
+
+    test('Given Temperature -50F When convert Then -45.55C', () => {
+        // GIVEN
+        let input = new TemperatureVO(-50, "FAHRENHEIT");
+        let expectedOutput = new TemperatureVO(-45.55555555555556, "CELSIUS");
+
+        // WHEN
+        let actualOutput = convert(input, "CELSIUS");
+
+        // THEN
+        expect(actualOutput).toStrictEqual(expectedOutput);
+    });
+    
+});
